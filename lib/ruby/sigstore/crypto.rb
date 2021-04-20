4efd278c3bb6d6ca98c27f1ffa0e3432ddf735fb
Luke Hinds <lhinds@redhat.com>
2021-04-20 16:30:42 +0100

    Wrong alg
    Signed-off-by: Luke Hinds <lhinds@redhat.com>

--- lib/ruby/sigstore/crypto.rb
+++ lib/ruby/sigstore/crypto.rb
@@ -3,7 +3,7 @@ require 'openssl'
 class Crypto 
     def initialize; end
     def generate_keys
-        key = OpenSSL::PKey::EC.new('secp256k1').generate_key
+        key = OpenSSL::PKey::EC.new('prime256v1').generate_key
         pkey = OpenSSL::PKey::EC.new(key.public_key.group)
         pkey.public_key = key.public_key
         return [key.to_pem, pkey.to_pem]
