c7e846a741e2b7288955937bed85194bb2fafafe
Luke Hinds <7058938+lukehinds@users.noreply.github.com>
2021-10-08 18:46:33 +0100

    Sign & Rekor implementation (#30)
    * Working signing and rekor entry
    
    Signed-off-by: Luke Hinds <lhinds@redhat.com>
    
    * Un-needed file open
    
    Signed-off-by: Luke Hinds <lhinds@redhat.com>
    
    * Signing and Rekor entry
    
    Signed-off-by: Luke Hinds <lhinds@redhat.com>
    
    * Working signing and rekor entry
    
    This signs all files. One last changed required is to pack the cert
    into data.tar.gz before its signed
    
    Signed-off-by: Luke Hinds <lhinds@redhat.com>
    
    * Correct cert path
    
    Signed-off-by: Luke Hinds <lhinds@redhat.com>

--- lib/rubygems/sigstore/crypto.rb
+++ lib/rubygems/sigstore/crypto.rb
@@ -19,10 +19,9 @@ class Crypto
     def initialize; end
 
     def generate_keys
-        key = OpenSSL::PKey::EC.new('prime256v1').generate_key
-        pkey = OpenSSL::PKey::EC.new(key.public_key.group)
-        pkey.public_key = key.public_key
-        return [key, Base64.encode64(pkey.to_der)]
+        key = OpenSSL::PKey::RSA.generate(2048)
+        pkey = key.public_key
+        return [key, pkey, Base64.encode64(pkey.to_der)]
     end
 
     def sign_proof(priv_key, email)
@@ -30,3 +29,20 @@ class Crypto
         return Base64.encode64(proof)
     end
 end
+
+# class Crypto 
+#     def initialize; end
+
+#     def generate_keys
+#         key = OpenSSL::PKey::EC.new('prime256v1').generate_key
+#         pkey = OpenSSL::PKey::EC.new(key.public_key.group)
+#         pkey.public_key = key.public_key
+#         return [key, pkey, Base64.encode64(pkey.to_der)]
+#     end
+
+#     def sign_proof(priv_key, email)
+#         proof = priv_key.sign(OpenSSL::Digest::SHA256.new, email)
+#         return Base64.encode64(proof)
+#     end
+# end
+
