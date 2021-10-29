b8f637acb7f26c5680fa47dd279c235d7087bb1e
rochlefebvre <roch.lefebvre@shopify.com>
2021-10-29 17:39:27 -0400

    super terrible `gem verify` implementation

--- lib/rubygems/sigstore/http_client.rb
+++ lib/rubygems/sigstore/http_client.rb
@@ -41,26 +41,55 @@ class HttpClient
       request.adapter :net_http
     end
     rekor_response = connection.post("/api/v1/log/entries",
-        {
-          kind: "rekord",
-            apiVersion: "0.0.1",
-            spec: {
-              signature: {
-                format: "x509",
-                    content: Base64.encode64(data_signature),
-                    publicKey: {
-                      content: Base64.encode64(cert_chain),
-                    },
-              },
-                data: {
-                  content: data_raw,
-                    hash: {
-                      algorithm: "sha256",
-                        value: data_digest,
-                    },
-                },
+      {
+        kind: "rekord",
+          apiVersion: "0.0.1",
+          spec: {
+            signature: {
+              format: "x509",
+                  content: Base64.encode64(data_signature),
+                  publicKey: {
+                    content: Base64.encode64(cert_chain),
+                  },
             },
-        })
+              data: {
+                content: data_raw,
+                  hash: {
+                    algorithm: "sha256",
+                      value: data_digest,
+                  },
+              },
+          },
+      })
     return rekor_response.body
+  end
+
+  def get_rekor_entries(data_digest, rekor_host)
+    # rekor uses a self signed certificate which fails the ssl check
+    connection = Faraday.new(ssl: { verify: false }) do |request|
+      request.url_prefix = rekor_host
+      request.request :json
+      request.response :json, content_type: /json/
+      request.adapter :net_http
+    end
+
+    retrieve_response = connection.post("/api/v1/index/retrieve",
+      {
+        hash: "sha256:#{data_digest}",
+      }
+    )
+
+    unless retrieve_response.status == 200
+      raise "Unexpected response from POST /api/v1/index/retrieve:\n #{retrieve_response}"
+    end
+
+    retrieve_response.body.map do |uuid|
+      entry_response = connection.get("api/v1/log/entries/#{uuid}")
+      unless entry_response.status == 200
+        raise "Unexpected response from GET api/v1/log/entries/#{uuid}:\n #{entry_response}"
+      end
+
+      entry_response.body
+    end
   end
 end
