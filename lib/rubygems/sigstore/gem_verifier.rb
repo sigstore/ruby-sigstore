714cda534599690fa92f5d9946ac388c7fc85b7f
rochlefebvre <roch.lefebvre@shopify.com>
2021-11-01 09:42:15 -0400

    print all unique emails from valid signature entries

--- lib/rubygems/sigstore/gem_verifier.rb
+++ lib/rubygems/sigstore/gem_verifier.rb
@@ -13,12 +13,13 @@ class Gem::Sigstore::GemVerifier
     rekor_api = Gem::Sigstore::RekorApi.new(host: config.rekor_host)
     entries = rekor_api.where(data_digest: gemfile.digest)
     rekord_entries = entries.map { |entry| Gem::Sigstore::RekordEntry.new(entry.values.first) }
-    rekord = rekord_entries.find { |entry| valid_signature?(entry, gemfile) }
+    rekords = rekord_entries.select { |entry| valid_signature?(entry, gemfile) }
 
-    if rekord
-      io.puts ":noice:, signed by #{rekord.signer_email}"
-    else
+    if rekords.empty?
       io.puts "not :noice: thxkbye"
+    else
+      io.puts ":noice:"
+      print_signers(rekords)
     end
   end
 
@@ -33,5 +34,25 @@ class Gem::Sigstore::GemVerifier
     content = gemfile.content
 
     public_key.verify(digest, signature, content)
+  end
+
+  def print_signers(rekords)
+    maintainers, others = rekords.map(&:signer_email).uniq.partition do |email|
+      gemfile.maintainer?(email)
+    end
+
+    unless maintainers.empty?
+      io.puts "Signed by maintainer#{maintainers.size == 1 ? '' : 's'}: #{email_list(maintainers)}"
+    end
+
+    unless others.empty?
+      io.puts "Signed by non-maintainer#{others.size == 1 ? '' : 's'}: #{email_list(others)}"
+    end
+  end
+
+  def email_list(emails)
+    return emails.first if emails.size == 1
+
+    emails[...-1].join(", ") + " and #{emails.last}"
   end
 end
