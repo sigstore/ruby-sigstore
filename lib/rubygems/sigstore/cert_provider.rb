97e3b3ff722a9dd91ecb09ef6c68e15b0b6af9ae
Frederik Dudzik <frederik.dudzik@shopify.com>
2021-11-23 10:20:50 -0800

    move openid class to openid::dynamic

--- lib/rubygems/sigstore/cert_provider.rb
+++ lib/rubygems/sigstore/cert_provider.rb
@@ -5,7 +5,7 @@ class Gem::Sigstore::CertProvider
   end
 
   def run
-    proof, access_token = Gem::Sigstore::OpenID.new(pkey.private_key).get_token
+    proof, access_token = Gem::Sigstore::OpenID::Dynamic.new(pkey.private_key).get_token
     fulcio_api = Gem::Sigstore::FulcioApi.new(token: access_token, host: config.fulcio_host)
     fulcio_api.create(proof, pkey.public_key.to_der)
   end
